c6cfe669af953b703c9ed09b0a9bfc7401901c10
Slidejiveman <rdnotlaw91@outlook.com>
2017-12-28 17:28:41 -0600

    changes

--- cuda_properties_errors/kernel.cu
+++ cuda_properties_errors/kernel.cu
@@ -9,14 +9,31 @@ int main()
 	// Fetch device properties and display them to the screen.
 	int nDevices;
 
-	cudaGetDeviceCount(&nDevices);
+	// All CUDA API calls have a return value that indicate
+	// whether or not an error occurred during the execution
+	// of the function.
+	cudaError_t err = cudaGetDeviceCount(&nDevices);
+	// Code like this will handle errors in the CPU calls.
+	// Kernel errors are more difficult to handle than this
+	// since they are executing asynchronously with respect
+	// to the host (CPU). Debugging macros in the in the 
+	// asynchronous portions of your code prevents concurrency,
+	// so be wise when checking those. Probably not good for 
+	// release builds.
+	if (err != cudaSuccess) {
+		printf("%s\n", cudaGetErrorString(err));
+	}
+
 	for (int i = 0; i < nDevices; i++) {
-		cudaDeviceProp prop;
+		cudaDeviceProp prop; // Note: this struct has many other fields
 		cudaGetDeviceProperties(&prop, i);
 		printf("Device Number: %d\n", i);
 		printf("  Device name: %s\n", prop.name);
 		printf("  Memory Clock Rate (KHz): %d\n", prop.memoryClockRate);
 		printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
+		
+		// This calculation represents the theoretical peak memory bandwidth
+		// which is based on the given hardware specs.
 		printf("  Peak Memory Bandwidth (GB/s): %f\n\n", 
 			2.0 * prop.memoryClockRate * (prop.memoryBusWidth / 8) / 1.0e6);
 	}
